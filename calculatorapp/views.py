64cd83e4a9abd5693eeb66567aec2ce196d7136f
Ai Lin Zuccarello <ailincz@LAPTOP-7BK8AHJC.winok.localnetwork>
2024-09-26 14:56:27 +0200

    fix: error message if input empty

--- calculatorapp/views.py
+++ calculatorapp/views.py
@@ -5,18 +5,26 @@ def home(request):
     return render(request, 'home.html')
 
 def result(request): 
-    num1 = int(request.GET.get('number1')) 
-    num2 = int(request.GET.get('number2'))
 
-    if request.GET.get('add') == "": ans = num1 + num2
+    num1 = request.GET.get('number1')
+    num2 = request.GET.get('number2')
 
-    elif request.GET.get('subtract') == "":
-        ans = num1 - num2
+    if not num1 or not num2:
+        ans = "Please insert both numbers :)"
+        
+    else:
+        num1=int(num1)
+        num2=int(num2)
 
-    elif request.GET.get('multiply') == "":
-        ans = num1 * num2
+        if request.GET.get('add') == "": ans = num1 + num2
 
-    else: 
-        ans = num1 / num2
+        elif request.GET.get('subtract') == "":
+            ans = num1 - num2
+
+        elif request.GET.get('multiply') == "":
+            ans = num1 * num2
+
+        else: 
+            ans = num1 / num2
 
     return render(request, 'result.html', {'ans': ans})
